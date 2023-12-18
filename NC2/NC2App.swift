633d12b8a1a4542063664aa4cd27d99fee77b290
iLujainAbdullah <lujain.abh@gmail.com>
2023-12-18 18:20:07 +0300

    Initial Commit

---
+++ NC2/NC2App.swift
@@ -0,0 +1,17 @@
+//
+//  NC2App.swift
+//  NC2
+//
+//  Created by Lujain Abdullah Halabi Almeri on 05/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct NC2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
