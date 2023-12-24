fcdd9fd20fddb0841f0a797866304ff8bdb5065a
iLujainAbdullah <lujain.abh@gmail.com>
2023-12-24 12:44:45 +0300

    commit

--- NC2/home.swift
+++ NC2/home.swift
@@ -1,38 +1,38 @@
-////
-////  home.swift
-////  NC2
-////
-////  Created by Lujain Abdullah Halabi Almeri on 06/06/1445 AH.
-////
 //
-//import SwiftUI
+//  home.swift
+//  NC2
 //
-//struct home: View {
-//    var body: some View {
-//        ZStack{
-//            
-//            Color("lColor")
-//                .edgesIgnoringSafeArea(.all)
-//           
-//            VStack{
-//                ScrollView{
-//                    CalendarView()
-//                    // view()
-//                  //  MealsDetails(currentDate: <#Binding<Date>#>)
-//                        .ignoresSafeArea(.keyboard)
-//                    Image("bg").resizable().frame(width: 450, height: 450).opacity(0.2)
-//                        .padding(.top, -20).padding(.bottom, -80)
-//                }//.padding(.top, 150)
-//            }
-////                .ignoresSafeArea(.keyboard)
-//            }/*.ignoresSafeArea(.keyboard)*/
-//        }
-//    }
-////}
+//  Created by Lujain Abdullah Halabi Almeri on 06/06/1445 AH.
 //
-//
-//
-//
-//#Preview {
-//    home()
+
+import SwiftUI
+
+struct home: View {
+    var body: some View {
+        ZStack{
+            
+            Color("lColor")
+                .edgesIgnoringSafeArea(.all)
+           
+            VStack{
+                ScrollView{
+                    CalendarView()
+                    // view()
+                    MealsDetails()
+                        .ignoresSafeArea(.keyboard)
+                    Image("bg").resizable().frame(width: 450, height: 450).opacity(0.2)
+                        .padding(.top, -20).padding(.bottom, -80)
+                }//.padding(.top, 150)
+            }
+//                .ignoresSafeArea(.keyboard)
+            }/*.ignoresSafeArea(.keyboard)*/
+        }
+    }
 //}
+
+
+
+
+#Preview {
+    home()
+}
--- NC2/mealsDetails.swift
+++ NC2/mealsDetails.swift
@@ -1,9 +1,3 @@
-//
-//  MealsModel.swift
-//  NC2
-//
-//  Created by Ghalia Mohammed Al Muaddi on 09/06/1445 AH.
-//
 ////
 ////  mealsDetails.swift
 ////  NC2
@@ -14,28 +8,40 @@
 
 import SwiftUI
 import WidgetKit
-import SwiftData
 
 struct MealsDetails: View {
     
-    @Environment(\.modelContext) var context
-    @Environment(\.dismiss) var dismiss
-    @Binding var currentDate2 : Date
-    @State var Breakfast : String = ""
-    @State var Lunch : String = ""
-    @State var Dinner : String = ""
-    @State var Snack : String = ""
+    @AppStorage(
+        "breakfast",
+        store: UserDefaults(
+            suiteName: "group.a.NC2"
+        )
+    ) private var breakfast = ""
     
+    @AppStorage(
+        "lunch",
+        store: UserDefaults(
+            suiteName: "group.a.NC2"
+        )
+    ) private var lunch = ""
+    
+    @AppStorage(
+        "dinner",
+        store: UserDefaults(
+            suiteName: "group.a.NC2"
+        )
+    ) private var dinner = ""
+    
+    @AppStorage(
+        "snack",
+        store: UserDefaults(
+            suiteName: "group.a.NC2"
+        )
+    ) private var snack = ""
     
     @State private var isButtonClicked = false
-    @State private var Show_Flage = true
-    @State private var data_flage = true
-    
-    
-    @Query var Meals : [MealsModel]
     
     var body: some View {
-        
         ZStack {
             Color("lColor")
                 .edgesIgnoringSafeArea(.all)
@@ -46,41 +52,12 @@ struct MealsDetails: View {
                 .cornerRadius(30)
                 .shadow(radius: 10)
             
-            
             VStack {
-                
                 Button(action: {
-                    // Action for the "Edit" button
-                  
-                    Show_Flage = false
                     isButtonClicked.toggle()
-
-                    // WidgetCenter.shared.reloadTimelines(ofKind: "widgetextension")
-                    
-                    
-                    
+                    WidgetCenter.shared.reloadTimelines(ofKind:"widgetextension")
                 }) {
-                    Image(systemName: "square.and.pencil")
-                        .resizable()
-                        .frame(width: 28, height: 28)
-                        .padding(.leading, 245)
-                        .foregroundColor(.blue)
-                        .cornerRadius(8)
-                        .foregroundColor(.white)
-                        .padding(.top, -20)
-                        .accessibilityLabel("Edit")
-                }.padding(.bottom, -25)
-                
-                if isButtonClicked {
-                    Button(action: {
-          // Action for the "Done" button
-                       
-                        
-                        save()
-                        // dismiss()
-                        // Other actions specific to the "Done" button
-                        
-                    }) {
+                    if isButtonClicked {
                         Text("Done")
                             .bold()
                             .padding(.vertical, 5)
@@ -88,199 +65,105 @@ struct MealsDetails: View {
                             .padding(.top, -20)
                             .padding(.leading, 235)
                             .accessibilityLabel("Done")
+                    } else {
+                        Image(systemName: "square.and.pencil")
+                            .resizable()
+                            .frame(width: 28, height: 28)
+                            .padding(.leading, 245)
+                            .foregroundColor(.blue)
+                            .cornerRadius(8)
+                            .foregroundColor(.white)
+                            .padding(.top, -20)
+                            .accessibilityLabel("Edit")
                     }
-                }
-                let lastMeal = Meals.last(where: { Calendar.current.compare($0.MealDay, to: currentDate2, toGranularity: .day) == .orderedSame })
+                }.padding(.bottom, -25)
+
+                HStack() {
+                    Image("breakfast").resizable()
+                        .frame(width: 38, height: 38)
+                    VStack(alignment: .leading) {
+                        Text("Breakfast")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color("gColor"))
+                        
+                        TextField("Type your breakfast", text: $breakfast)
+                            .textFieldStyle(CustomTextFieldStyle(isButtonClicked: isButtonClicked)).disabled(!isButtonClicked)
+                            .padding(.top, -14)
+                            .accessibilityLabel(!isButtonClicked ? "Click edit button to" : "")
+                            .ignoresSafeArea(.keyboard)
+                    }
+                }.padding(.bottom, 5)
                 
-                if (Show_Flage && (lastMeal != nil)) { // هنا بيعرض البيانات المطابقه للتاريخ
-                        //Text("\(currentDate2)")
-                        //  ForEach(Meals.filter { $0.MealDay == currentDate2 }) { meal in
+                HStack() {
+                    Image("lunch").resizable()
+                        .frame(width: 38, height: 38)
+                    VStack(alignment: .leading) {
+                        Text("Lunch")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color("gColor"))
                         
-//                        if let lastMeal = Meals.last(where: { Calendar.current.compare($0.MealDay, to: currentDate2, toGranularity: .day) == .orderedSame }) {
-//                            
-                            
-                            
-                            // Text("\(meal.MealDay)")
-                            HStack() {
-                                Image("breakfast").resizable()
-                                    .frame(width: 38, height: 38)
-                                VStack(alignment: .leading) {
-                                    Text("Breakfast")
-                                        .fontWeight(.semibold)
-                                        .foregroundColor(Color("gColor"))
-                                    Text(" \(lastMeal!.breakfast)")
-                                 //.textFieldStyle(CustomTextFieldStyle(isButtonClicked: isButtonClicked)).disabled(!isButtonClicked)
-                                        .padding(.top, -14)
-                                     .accessibilityLabel(!isButtonClicked ? "Click edit button to" : "")
-                                    .ignoresSafeArea(.keyboard)
-                                    
-                                    
-                                }
-                            }.padding(.bottom, 5)
-                            
-                            HStack() {
-                                Image("lunch").resizable()
-                                    .frame(width: 38, height: 38)
-                                VStack(alignment: .leading) {
-                                    Text("Lunch")
-                                        .fontWeight(.semibold)
-                                        .foregroundColor(Color("gColor"))
-                               
-                                    Text(lastMeal!.lunch)
-                                 
-                                        .padding(.top, -14)
-                                        .accessibilityLabel(!isButtonClicked ? "Click edit button to" : "")
-                                        .ignoresSafeArea(.keyboard)
-                                }
-                            }.padding(.bottom, 5)
-                            
-                            HStack() {
-                                Image("dinner").resizable()
-                                    .frame(width: 38, height: 38)
-                                VStack(alignment: .leading) {
-                                    Text("Dinner")
-                                        .fontWeight(.semibold)
-                                        .foregroundColor(Color("gColor"))
-                                    Text(lastMeal!.dinner)
-                                  
-                                        .padding(.top, -14)
-                                        .accessibilityLabel(!isButtonClicked ? "Click edit button to" : "")
-                                        .ignoresSafeArea(.keyboard)
-                                }
-                            }.padding(.bottom, 5)
-                            
-                            HStack() {
-                                Image("snack").resizable()
-                                    .frame(width: 38, height: 38).padding(.top, 10)
-                                VStack(alignment: .leading) {
-                                    Text("Snack")
-                                        .fontWeight(.semibold)
-                                        .foregroundColor(Color("gColor"))
-                                    Text(lastMeal!.snack)
-                                    
-                                   
-                                        .padding(.top, -14).ignoresSafeArea(.keyboard)
-                                        .accessibilityLabel((!isButtonClicked && (Snack != "")) ? "Click edit button to" : "")
-                                    
-                                }
-                            }.padding(.top, 10)
-                          
-                        //} //end of if for the calender condition
-//                        else {
-//                            
-//                            // Handle the case where no matching meal is found
-//                            Text("No meal available for the selected date")
-//                            
-//                        } //End of elase
-                        
-                    } //end of Show_Flage
-                    
-                  
-                    else { //اذا كان بيدخل بيانات
-                        HStack() {
-                            Image("breakfast").resizable()
-                                .frame(width: 38, height: 38)
-                            VStack(alignment: .leading) {
-                                Text("Breakfast")
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(Color("gColor"))
-                                
-                                TextField("Type your breakfast", text: $Breakfast)
-                                    .textFieldStyle(CustomTextFieldStyle(isButtonClicked: isButtonClicked)).disabled(!isButtonClicked)
-                                    .padding(.top, -14)
-                                    .accessibilityLabel(!isButtonClicked ? "Click edit button to" : "")
-                                    .ignoresSafeArea(.keyboard)
-                            }
-                        }.padding(.bottom, 5)
-                        
-                        HStack() {
-                            Image("lunch").resizable()
-                                .frame(width: 38, height: 38)
-                            VStack(alignment: .leading) {
-                                Text("Lunch")
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(Color("gColor"))
-                                
-                                TextField("Type your lunch", text: $Lunch)
-                                    .textFieldStyle(CustomTextFieldStyle(isButtonClicked: isButtonClicked))
-                                    .disabled(!isButtonClicked).padding(.top, -14)
-                                    .accessibilityLabel(!isButtonClicked ? "Click edit button to" : "")
-                                    .ignoresSafeArea(.keyboard)
-                            }
-                        }.padding(.bottom, 5)
-                        
-                        HStack() {
-                            Image("dinner").resizable()
-                                .frame(width: 38, height: 38)
-                            VStack(alignment: .leading) {
-                                Text("Dinner")
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(Color("gColor"))
-                                TextField("Type your dinner", text: $Dinner)
-                                    .textFieldStyle(CustomTextFieldStyle(isButtonClicked: isButtonClicked))
-                                    .disabled(!isButtonClicked).padding(.top, -14)
-                                    .accessibilityLabel(!isButtonClicked ? "Click edit button to" : "")
-                                    .ignoresSafeArea(.keyboard)
-                            }
-                        }.padding(.bottom, 5)
-                        
-                        HStack() {
-                            Image("snack").resizable()
-                                .frame(width: 38, height: 38).padding(.top, 10)
-                            VStack(alignment: .leading) {
-                                Text("Snack")
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(Color("gColor"))
-                                TextField("Type your snacks", text: $Snack)
-                                    .ignoresSafeArea(.keyboard)
-                                    .textFieldStyle(CustomTextFieldStyle(isButtonClicked: isButtonClicked)).disabled(!isButtonClicked)
-                                    .padding(.top, -14)
-                                    .accessibilityLabel((!isButtonClicked && (Snack != "")) ? "Click edit button to" : "")
-                                
-                            }
-                        }.padding(.top, 10)
-                        
-                    } //End of elas for adding data
-                    
-                    
-                    
-                  
-                    
-                    
-                } //End of Vstack
+                        TextField("Type your lunch", text: $lunch)
+                            .textFieldStyle(CustomTextFieldStyle(isButtonClicked: isButtonClicked))
+                            .disabled(!isButtonClicked).padding(.top, -14)
+                            .accessibilityLabel(!isButtonClicked ? "Click edit button to" : "")
+                            .ignoresSafeArea(.keyboard)
+                    }
+                }.padding(.bottom, 5)
                 
-                Divider().frame(width: 270, height: 0.4)
-                    .overlay(Color("bColor")).padding(.top, 135)
+                HStack() {
+                    Image("dinner").resizable()
+                        .frame(width: 38, height: 38)
+                    VStack(alignment: .leading) {
+                        Text("Dinner")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color("gColor"))
+                        TextField("Type your dinner", text: $dinner)
+                            .textFieldStyle(CustomTextFieldStyle(isButtonClicked: isButtonClicked))
+                            .disabled(!isButtonClicked).padding(.top, -14)
+                            .accessibilityLabel(!isButtonClicked ? "Click edit button to" : "")
+                            .ignoresSafeArea(.keyboard)
+                    }
+                }.padding(.bottom, 5)
                 
+                HStack() {
+                    Image("snack").resizable()
+                        .frame(width: 38, height: 38).padding(.top, 10)
+                    VStack(alignment: .leading) {
+                        Text("Snack")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color("gColor"))
+                        TextField("Type your snacks", text: $snack)
+                            .ignoresSafeArea(.keyboard)
+                            .textFieldStyle(CustomTextFieldStyle(isButtonClicked: isButtonClicked)).disabled(!isButtonClicked)
+                            .padding(.top, -14)
+                            .accessibilityLabel((!isButtonClicked && (snack != "")) ? "Click edit button to" : "")
+                        
+                    }
+                }.padding(.top, 10)
             }
             
-       
-        } //End of view
-        
-        
-        func save()  {
-            
-            let New_Meal = MealsModel(breakfast: Breakfast, lunch: Lunch, dinner: Dinner, snack: Snack, MealDay: currentDate2)
-            context.insert(New_Meal)
-            Show_Flage = true
-            //return true
+                Divider().frame(width: 270, height: 0.4)
+                    .overlay(Color("bColor")).padding(.top, 135)
+
         }
     }
+}
 
-    struct CustomTextFieldStyle: TextFieldStyle {
-        var isButtonClicked: Bool
-        
-        func _body(configuration: TextField<Self._Label>) -> some View {
-            configuration
-                .frame(width: 200, height: 30)
-                .background(RoundedRectangle(cornerRadius: 8)
-                    .stroke(isButtonClicked ? Color.gray : .clear, lineWidth: 1))
-                .foregroundColor(isButtonClicked ? Color.gray : Color("bColor"))
-        }
+struct CustomTextFieldStyle: TextFieldStyle {
+    var isButtonClicked: Bool
+    
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .frame(width: 200, height: 30) .padding(isButtonClicked ? EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 0) : EdgeInsets())
+
+            .background(RoundedRectangle(cornerRadius: 8)
+                .stroke(isButtonClicked ? Color.gray : .clear, lineWidth: 1))
+            .foregroundColor(isButtonClicked ? Color.gray : Color("bColor"))
     }
+}
 
-//struct MealsDetails_Previews: PreviewProvider {
-//    static var previews: some View {
-//        MealsDetails(currentDate2: Binding<Date>)
-//    }
-//}
+struct MealsDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        MealsDetails()
+    }
+}
