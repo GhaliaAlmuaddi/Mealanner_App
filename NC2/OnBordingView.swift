33f7fd500af04637f1bc91b6f8749fa02a8dff6f
Ghalia Al Muaddi <ghaliaalmuaddi@gmail.com>
2023-12-25 11:20:02 +0300

    Created the Onbording Screens

---
+++ NC2/OnBordingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBordingView.swift
+//  NC2
+//
+//  Created by Ghalia Mohammed Al Muaddi on 12/06/1445 AH.
+//
+
+import SwiftUI
+
+struct OnBordingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBordingView()
+}
