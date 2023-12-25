7ba41a373b2cd99a4e197dee4c1de907021a10f5
Ghalia Al Muaddi <ghaliaalmuaddi@gmail.com>
2023-12-25 18:55:17 +0300

    Added SplashView

---
+++ NC2/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  NC2
+//
+//  Created by Ghalia Mohammed Al Muaddi on 12/06/1445 AH.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
